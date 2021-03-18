36e708c008da842321bbff525791e20796bbd2a4
Tejas Chheda <tchheda@umass.edu>
2021-03-18 13:57:57 -0400

    Added Mapper code with custom key-value-pair class

--- src/Constants/MRConstant.java
+++ src/Constants/MRConstant.java
@@ -5,4 +5,5 @@ public class MRConstant {
     public static String MAPPER = "Mapper";
     public static String REDUCER = "Reducer";
     public static final String MAP_OUTPUT_DIR = "tmp/";
+    public static final String FILE_WRITE_EXCEPTION = "File write exception occured";
 }
