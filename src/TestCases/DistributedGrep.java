ca3aacd664bd84c22ee2150f826b45002fb85a2b
Tejas Chheda <tejas4888@gmail.com>
2021-03-26 12:36:02 -0400

    Removed small bug

--- src/TestCases/DistributedGrep.java
+++ src/TestCases/DistributedGrep.java
@@ -1,4 +1,19 @@
 package TestCases;
 
-public class DistributedGrep {
+import DataType.StringComp;
+import Model.Output;
+import Service.UDFInterface;
+
+public class DistributedGrep implements UDFInterface<StringComp, StringComp, StringComp, StringComp>
+{
+    String pattern="";
+    @Override
+    public void map(StringComp stringComp, StringComp stringComp2, Output output) {
+
+    }
+
+    @Override
+    public void reduce(StringComp stringComp, Iterable<StringComp> valueIter, Output output) {
+
+    }
 }
