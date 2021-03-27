8ce4e2675edd9f0a8420f1615e223d40f2a03dfb
parin1995 <prhjhaveri@gmail.com>
2021-03-26 22:25:37 -0400

    test scripts testing

--- python/inverted_index.py
+++ python/inverted_index.py
@@ -8,7 +8,7 @@ words = sc.textFile(par_dir+"/data/demo.txt").flatMap(lambda line: line.split(" 
 
 wordCounts = words.map(lambda word: (word, 3)).reduceByKey(lambda a,b:str(a)+","+str(b))
 
-f = open("inverted_index_output.txt","w")
+f = open("InvertedIndex.txt","w")
 for k,v in wordCounts.collect():
 	f.write(str(k)+" "+str(v))
 	f.write("\n")
--- python/word_count.py
+++ python/word_count.py
@@ -8,7 +8,7 @@ words = sc.textFile(par_dir+"/data/demo.txt").flatMap(lambda line: line.split(" 
 
 wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
 
-f = open("word_count_output.txt","w")
+f = open("WordCount.txt","w")
 for k,v in wordCounts.collect():
 	f.write(str(k)+" "+str(v))
 	f.write("\n")
