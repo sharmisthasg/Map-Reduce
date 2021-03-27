eba0482e03c51d90f3e5da5902e6c6e8b78507d2
Tejas Chheda <tejas4888@gmail.com>
2021-03-26 22:27:43 -0400

    Completed inverted index python script

--- pyspark/inverted_index.py
+++ pyspark/inverted_index.py
@@ -2,15 +2,27 @@ import sys,os
 from pyspark import SparkContext, SparkConf
 
 par_dir=os.path.split(os.getcwd())[0]
+di={}
 
-sc = SparkContext("local","PySpark Word Count Exmaple")
-words = sc.textFile(par_dir+"/data/demo.txt").flatMap(lambda line: line.split(" "))
+f = open(par_dir+"/data/demo.txt","r")
+for i,line in enumerate(f):
+	line = line.replace("\n","")
+	words = line.split(" ")
+	for word in words:
+		if word in di:
+			di[word].append(i)
+		else:
+			di[word]=[i]
 
-wordCounts = words.map(lambda word: (word, 3)).reduceByKey(lambda a,b:str(a)+","+str(b))
+f.close()
 
 f = open("inverted_index_output.txt","w")
-for k,v in wordCounts.collect():
-	f.write(str(k)+" "+str(v))
+for word in di:
+	f.write(word+" ")
+	for i,index in enumerate(di[word]):
+		f.write(str(index))
+		if i!=(len(di[word])-1):
+			f.write(",")
 	f.write("\n")
 f.close()
 
